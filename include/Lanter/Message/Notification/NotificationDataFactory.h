879ebe1e43dd655161e141ed047e87703bd1697f
Лысенков Владимир <vladimir-lysenkov93@yandex.ru>
2021-08-30 20:16:22 +0300

    feature(NotificationDataFactory) Добавлен фабричный метод с параметром
    Добавлен фабричный метод с параметром, чтобы при конструировании объекта
    автоматически задавалось поле NotificationCode

--- include/Lanter/Message/Notification/NotificationDataFactory.h
+++ include/Lanter/Message/Notification/NotificationDataFactory.h
@@ -12,9 +12,15 @@ namespace Lanter {
             /// \brief Фабричный класс, возвращающий объект уведомления
             class LANTER_VISIBILITY NotificationDataFactory {
             public:
+                /// \brief Возвращает указатель на объект интерфейса INotificationData и заполняет поле NotificationCode
+                /// \param[in] code Код уведомления
+                /// \return Указатель на объект интерфейса INotificationData
+                static std::shared_ptr<INotificationData> getNotificationData(NotificationCode code);
+
                 /// \brief Возвращает указатель на объект интерфейса INotificationData
                 /// \return Указатель на объект интерфейса INotificationData
                 static std::shared_ptr<INotificationData> getNotificationData();
+
             };//class NotificationDataFactory
         }//namespace Notification
     }//namespace Message
--- src/Lanter/Message/Notification/NotificationDataFactory.cpp
+++ src/Lanter/Message/Notification/NotificationDataFactory.cpp
@@ -6,9 +6,20 @@ namespace Lanter {
     namespace Message {
         namespace Notification {
 
+            std::shared_ptr<INotificationData> NotificationDataFactory::getNotificationData(NotificationCode code) {
+                auto notification = std::make_shared<NotificationData>();
+
+                if(notification != nullptr) {
+                    notification->setCode(code);
+                }
+
+                return notification;
+            }
+
             std::shared_ptr <INotificationData> NotificationDataFactory::getNotificationData() {
-                return std::make_shared<NotificationData>();
-            }//getNotificationData()
+                return getNotificationData(NotificationCode::NoNotification);
+            }
+//getNotificationData()
         }//namespace Notification
     }//namespace Message
 }//namespace Lanter
