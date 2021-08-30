3ac70c7762d393c410d73160268abe723ffae4db
Лысенков Владимир <vladimir-lysenkov93@yandex.ru>
2021-08-30 20:22:33 +0300

    test(Lan4Gate) Расширены тесты в соотвествтии с интерфейсом
    Расширены тесты в соответствии с новыми методами интерфейса ILan4Gate

--- tests/unit_tests/Impl_tests/Manager/Lan4Gate.cpp
+++ tests/unit_tests/Impl_tests/Manager/Lan4Gate.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include "Lanter/Utils/Constants.h"
+
 #include "Lanter/Manager/Lan4Gate.h"
 
 using namespace Lanter;
@@ -14,66 +16,6 @@ using namespace Lanter::Message::Response;
 using namespace Lanter::Message::Notification;
 
 using testing::Return;
-
-class MOCKComms : public ICommunication {
-public:
-    MOCK_METHOD(bool, open, (), (override));
-    MOCK_METHOD(bool, close, (), (override));
-    MOCK_METHOD(bool, isOpen, (), (override));
-
-    MOCK_METHOD(size_t, send, (const std::vector<uint8_t> &), (override));
-    MOCK_METHOD(size_t, receive, (std::vector<uint8_t> &), (override));
-};
-
-TEST(Lan4Gate, TestAddResetGetCommunication) {
-    //закроется с первой попытки
-    auto closedComms = std::make_shared<MOCKComms>();
-    EXPECT_CALL(*closedComms, isOpen).
-        WillRepeatedly(Return(false));
-
-    auto simpleOpen = std::make_shared<MOCKComms>();
-
-    EXPECT_CALL(*simpleOpen, isOpen).
-            WillOnce(Return(true)).
-            WillRepeatedly(Return(false)); //последующие закроют
-
-    EXPECT_CALL(*simpleOpen, close).
-            WillRepeatedly(Return(true)); //вызов close сразу вернет true
-
-
-    auto lockedComms = std::make_shared<MOCKComms>();
-
-    EXPECT_CALL(*lockedComms, isOpen).
-        WillOnce(Return(true)).
-        WillOnce(Return(true)).
-        WillRepeatedly(Return(false));
-
-    EXPECT_CALL(*lockedComms, close).
-        WillOnce(Return(false)).
-        WillOnce(Return(false)).
-        WillRepeatedly(Return(true));
-
-    Lan4Gate gate;
-
-    EXPECT_TRUE(gate.setCommunication(closedComms));
-    EXPECT_EQ(gate.getCommunication(), closedComms);
-    EXPECT_TRUE(gate.resetCommunication());
-    EXPECT_EQ(gate.getCommunication(), nullptr);
-
-
-    EXPECT_TRUE(gate.setCommunication(simpleOpen));
-    EXPECT_EQ(gate.getCommunication(), simpleOpen);
-    EXPECT_TRUE(gate.resetCommunication());
-    EXPECT_EQ(gate.getCommunication(), nullptr);
-
-
-    EXPECT_TRUE(gate.setCommunication(lockedComms));
-    EXPECT_FALSE(gate.resetCommunication());
-    EXPECT_FALSE(gate.resetCommunication());
-    EXPECT_TRUE(gate.resetCommunication());
-    EXPECT_EQ(gate.getCommunication(), nullptr);
-}
-
 
 TEST(Lan4Gate, TestAddRemoveRequestCallback) {
     int iterationCount = 50;
@@ -171,3 +113,174 @@ TEST(Lan4Gate, TestAddRemoveNotificationCallback) {
 
 }
 
+class MOCKComms : public ICommunication {
+public:
+    MOCK_METHOD(bool, open, (), (override));
+    MOCK_METHOD(bool, close, (), (override));
+    MOCK_METHOD(bool, isOpen, (), (override));
+
+    MOCK_METHOD(size_t, send, (const std::vector<uint8_t> &), (override));
+    MOCK_METHOD(size_t, receive, (std::vector<uint8_t> &), (override));
+};
+
+TEST(Lan4Gate, TestAddResetGetCommunication) {
+    //закроется с первой попытки
+    auto closedComms = std::make_shared<MOCKComms>();
+    EXPECT_CALL(*closedComms, isOpen).
+            WillRepeatedly(Return(false));
+
+    auto simpleOpen = std::make_shared<MOCKComms>();
+
+    EXPECT_CALL(*simpleOpen, isOpen).
+            WillOnce(Return(true)).
+            WillRepeatedly(Return(false)); //последующие закроют
+
+    EXPECT_CALL(*simpleOpen, close).
+            WillRepeatedly(Return(true)); //вызов close сразу вернет true
+
+
+    auto lockedComms = std::make_shared<MOCKComms>();
+
+    EXPECT_CALL(*lockedComms, isOpen).
+            WillOnce(Return(true)).
+            WillOnce(Return(true)).
+            WillRepeatedly(Return(false));
+
+    EXPECT_CALL(*lockedComms, close).
+            WillOnce(Return(false)).
+            WillOnce(Return(false)).
+            WillRepeatedly(Return(true));
+
+    Lan4Gate gate;
+
+    EXPECT_TRUE(gate.setCommunication(closedComms));
+    EXPECT_EQ(gate.getCommunication(), closedComms);
+    EXPECT_TRUE(gate.resetCommunication());
+    EXPECT_EQ(gate.getCommunication(), nullptr);
+
+
+    EXPECT_TRUE(gate.setCommunication(simpleOpen));
+    EXPECT_EQ(gate.getCommunication(), simpleOpen);
+    EXPECT_TRUE(gate.resetCommunication());
+    EXPECT_EQ(gate.getCommunication(), nullptr);
+
+
+    EXPECT_TRUE(gate.setCommunication(lockedComms));
+    EXPECT_FALSE(gate.resetCommunication());
+    EXPECT_FALSE(gate.resetCommunication());
+    EXPECT_TRUE(gate.resetCommunication());
+    EXPECT_EQ(gate.getCommunication(), nullptr);
+}
+
+TEST(Lan4Gate, TestStartStop) {
+    auto stubComms = std::make_shared<MOCKComms>();
+
+    Lan4Gate gate;
+    gate.setCommunication(stubComms);
+
+    EXPECT_FALSE(gate.isStarted());
+    EXPECT_TRUE(gate.start());
+    EXPECT_TRUE(gate.isStarted());
+    EXPECT_TRUE(gate.stop());
+    EXPECT_FALSE(gate.isStarted());
+}
+
+TEST(Lan4Gate, TestEcrNumber) {
+    auto stubComms = std::make_shared<MOCKComms>();
+
+    int validEcrNumber = Utils::Constants::MINIMUM_ECR_NUMBER;
+    int invalidEcrNumber = Utils::Constants::MAXIMUM_ECR_NUMBER + 1;
+
+    Lan4Gate gate;
+
+    EXPECT_EQ(gate.getEcrNumber(), 1);
+
+    EXPECT_TRUE(gate.setEcrNumber(validEcrNumber));
+    EXPECT_FALSE(gate.setEcrNumber(invalidEcrNumber));
+    EXPECT_EQ(gate.getEcrNumber(), 1);
+
+
+    gate.setCommunication(stubComms);
+
+    EXPECT_TRUE(gate.start());
+
+    EXPECT_FALSE(gate.setEcrNumber(validEcrNumber));
+    EXPECT_FALSE(gate.setEcrNumber(invalidEcrNumber));
+
+    EXPECT_TRUE(gate.stop());
+
+    EXPECT_TRUE(gate.setEcrNumber(validEcrNumber));
+    EXPECT_FALSE(gate.setEcrNumber(invalidEcrNumber));
+    EXPECT_EQ(gate.getEcrNumber(), 1);
+}
+
+TEST(Lan4Gate, TestPreparedRequest) {
+    int validEcrNumber = Utils::Constants::MINIMUM_ECR_NUMBER;
+
+    Lan4Gate gate;
+    gate.setEcrNumber(validEcrNumber);
+
+    auto lessNoOperation = (OperationCode)((int)OperationCode::NoOperation - 1);
+
+    auto invalidRequest = gate.getPreparedRequest(lessNoOperation);
+
+    auto validRequest = gate.getPreparedRequest(OperationCode::Sale);
+
+    EXPECT_EQ(invalidRequest, nullptr);
+
+    ASSERT_NE(validRequest, nullptr);
+
+    EXPECT_EQ(validRequest->getEcrNumber(), validEcrNumber);
+
+    EXPECT_EQ(validRequest->getOperationCode(), OperationCode::Sale);
+
+
+}
+
+TEST(Lan4Gate, TestPreparedResponse) {
+    int validEcrNumber = Utils::Constants::MINIMUM_ECR_NUMBER;
+
+    Lan4Gate gate;
+    gate.setEcrNumber(validEcrNumber);
+
+    auto lessNoOperation = (OperationCode)((int)OperationCode::NoOperation - 1);
+
+    auto invalidResponse = gate.getPreparedResponse(lessNoOperation);
+
+    auto validResponse = gate.getPreparedResponse(OperationCode::Sale);
+
+    EXPECT_EQ(invalidResponse, nullptr);
+
+    ASSERT_NE(validResponse, nullptr);
+
+    EXPECT_EQ(validResponse->getEcrNumber(), validEcrNumber);
+
+    EXPECT_EQ(validResponse->getOperationCode(), OperationCode::Sale);
+}
+
+TEST(Lan4Gate, TestPreparedNotification) {
+    std::shared_ptr<ILan4Gate> gate = std::make_shared<Lan4Gate>();
+
+    ASSERT_NE(gate, nullptr);
+
+    auto firstValue = gate->getPreparedNotification(NotificationCode::FirstValue);
+
+    auto lastValue = gate->getPreparedNotification(NotificationCode::LastValue);
+
+    auto noCode = gate->getPreparedNotification(NotificationCode::NoNotification);
+
+    auto withoutParam = gate->getPreparedNotification();
+
+
+    ASSERT_NE(firstValue, nullptr);
+    EXPECT_EQ(firstValue->getCode(), NotificationCode::FirstValue);
+
+    ASSERT_NE(lastValue, nullptr);
+    EXPECT_EQ(lastValue->getCode(), NotificationCode::LastValue);
+
+    ASSERT_NE(noCode, nullptr);
+    EXPECT_EQ(noCode->getCode(), NotificationCode::NoNotification);
+
+    ASSERT_NE(withoutParam, nullptr);
+    EXPECT_EQ(withoutParam->getCode(), NotificationCode::NoNotification);
+}
