04dcb85e71cb58f06d085b7fea61cddaaa391226
Лысенков Владимир <vladimir-lysenkov93@yandex.ru>
2021-08-30 08:57:35 +0300

    feature(ICommunication) Добавлен интерфейс для коммуникаций

--- include/Lanter/Communication/ICommunication.h
+++ include/Lanter/Communication/ICommunication.h
@@ -1,15 +1,37 @@
-//
-// Created by Vladimir on 27.01.2021.
-//
-
 #ifndef LAN_4GATELIB_ICOMMUNICATION_H
 #define LAN_4GATELIB_ICOMMUNICATION_H
 
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
 namespace Lanter {
     namespace Communication {
-        ///\brief
+        ///\brief Интерфейс класса коммуникаций, используемый в работе Lan4Gate
+        /// \sa Lan4Gate
         class ICommunication {
+        public:
+            /// \brief Открывает соединение
+            /// \return true, если соединение успешно открыто
+            virtual bool open() = 0;
 
+            /// \brief Закрывает соединение
+            /// \return true, если соединение успешно закрыто
+            virtual bool close() = 0;
+
+            /// \brief Возвращает признак открытого соединения
+            /// \return true - открыто, false - закрыто
+            virtual bool isOpen() = 0;
+
+            /// \brief Отправляет данные в соединение
+            /// \param[in] in отправляемые данные
+            /// \return размер отправленных данных
+            virtual size_t send(const std::vector<uint8_t> & in) = 0;
+
+            /// \brief Принимает данные из соединения
+            /// \param out контейнер для полученных данных
+            /// \return размер полученных данных
+            virtual size_t receive(std::vector<uint8_t> & out) = 0;
         };//class ICommunication
     }//namespace Communication
 }//namespace Lanter
