d051354df406cb3a017faa32193e8de43f7239af
Лысенков Владимир <vladimir-lysenkov93@yandex.ru>
2021-08-30 20:21:45 +0300

    test(NotificationDataFactory) Расширен тест для нового метода

--- tests/unit_tests/InterfaceTest/Message/NotificationDataFactory.cpp
+++ tests/unit_tests/InterfaceTest/Message/NotificationDataFactory.cpp
@@ -9,5 +9,30 @@ using namespace Lanter::Message::Notification;
 using namespace Lanter::Utils::Constants;
 
 TEST(NotificationDataFactory, CheckFactory) {
-    EXPECT_TRUE(NotificationDataFactory::getNotificationData() != nullptr);
+    auto lessFirstOperation = (NotificationCode)((int)NotificationCode::FirstValue - 1);
+
+    auto greatLastOperation = (NotificationCode)((int)NotificationCode::LastValue + 1);
+
+    auto lessNotification = NotificationDataFactory::getNotificationData(lessFirstOperation);
+    auto greatNotification = NotificationDataFactory::getNotificationData(greatLastOperation);
+
+    auto firstNotification = NotificationDataFactory::getNotificationData(NotificationCode::FirstValue);
+    auto lastNotification = NotificationDataFactory::getNotificationData(NotificationCode::LastValue);
+
+    EXPECT_NE(NotificationDataFactory::getNotificationData(), nullptr);
+
+
+    ASSERT_NE(lessNotification, nullptr);
+    EXPECT_EQ(lessNotification->getCode(), NotificationCode::NoNotification);
+
+    ASSERT_NE(greatNotification, nullptr);
+    EXPECT_EQ(greatNotification->getCode(), NotificationCode::NoNotification);
+
+
+    ASSERT_NE(firstNotification, nullptr);
+    EXPECT_EQ(firstNotification->getCode(), NotificationCode::FirstValue);
+
+    ASSERT_NE(lastNotification, nullptr);
+    EXPECT_EQ(lastNotification->getCode(), NotificationCode::LastValue);
+
 }
