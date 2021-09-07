9fa66ca2e17096cbd22231797111b78ba45865a7
Vladimir Lysenkov <vladimir-lysenkov93@yandex.ru>
2021-09-07 05:43:53 +0300

    feature(Utils) в Utils Добавлена функция для получения версии библиотеки

---
+++ include/Lanter/Utils/Version.h
@@ -0,0 +1,12 @@
+#ifndef LAN_4GATELIB_VERSION_H
+#define LAN_4GATELIB_VERSION_H
+
+#include "string"
+
+namespace Lanter {
+    namespace Utils {
+        const std::string & getVersion();
+    }
+}
+
+#endif //LAN_4GATELIB_VERSION_H
---
+++ src/Lanter/Utils/Version.cpp
@@ -0,0 +1,4 @@
+//
+// Created by pokem on 07.09.2021.
+//
+
