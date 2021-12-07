0c407ea2cda5ea4cf186b3f4248f5df813cad22c
Vladimir Lysenkov <vladimir-lysenkov93@yandex.ru>
2021-12-07 17:27:33 +0300

    test(Impl/MessageBuilder) Добавлен тест сборщика сообщений Interaction

---
+++ tests/unit_tests/Impl_tests/MessageProcessor/MessageBuilder/TestJSONInteractionBuilder.cpp
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+
+#include "Lanter/Message/Interaction/InteractionDataFactory.h"
+
+#include "Lanter/MessageProcessor/Builder/JSONInteractionBuilder.h"
+
+#include "Lanter/Message/Interaction/InteractionData.h"
+
+#include "Lanter/MessageProcessor/JSONMessageFields.h"
+
+#include "Lanter/MessageProcessor/JSONFieldExists.h"
+
+using namespace Lanter;
+using namespace Lanter::Interaction;
+using namespace Lanter::Message;
+using namespace Lanter::MessageProcessor;
+using namespace Lanter::MessageProcessor::Builder;
+
+TEST(TestJSONInteractionBuilder, CheckAddCode) {
+    InteractionData data;
+
+    Json::Value object;
+
+    EXPECT_FALSE(JSONInteractionBuilder::addCode(data, object));
+
+    data.setCode(InteractionCode::FirstValue);
+
+    EXPECT_TRUE(JSONInteractionBuilder::addCode(data, object));
+    EXPECT_TRUE(fieldExists(object, JSONInteractionFields::getCode()));
+    EXPECT_EQ((int) data.getCode(), object[JSONInteractionFields::getCode()].asInt());
+}
+
+TEST(TestJSONInteractionBuilder, CheckCreateObject) {
+    auto data = InteractionDataFactory::getInteractionData();
+    Json::Value object;
+    JSONInteractionBuilder builder;
+
+    EXPECT_FALSE(builder.createObject(*data, object));
+
+    data->setCode(InteractionCode::FirstValue);
+
+    EXPECT_TRUE(builder.createObject(*data, object));
+    EXPECT_TRUE(fieldExists(object, JSONInteractionFields::getCode()));
+
+    EXPECT_EQ((int) data->getCode(), object[JSONInteractionFields::getCode()].asInt());
+}
