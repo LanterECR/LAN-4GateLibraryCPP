7c30211cfd52c1047f8308e7dc5842e849022c1d
Vladimir Lysenkov <vladimir-lysenkov93@yandex.ru>
2021-12-13 11:54:23 +0300

    docs Обновлен Doxygen в соотвествтии с новой версией

--- doxygen/search/all_12.js
+++ doxygen/search/all_12.js
@@ -3,8 +3,9 @@ var searchData=
   ['validatemandatoryfields_0',['validateMandatoryFields',['../class_lanter_1_1_message_1_1_request_1_1_i_request_data.html#a772013fe12aa5312c6c2042ac8a515bf',1,'Lanter::Message::Request::IRequestData::validateMandatoryFields()'],['../class_lanter_1_1_message_1_1_response_1_1_i_response_data.html#abf98b7d85dd1bdccb3a8af0c3527d104',1,'Lanter::Message::Response::IResponseData::validateMandatoryFields()']]],
   ['version_1',['Version',['../class_lanter_1_1_utils_1_1_version.html',1,'Lanter::Utils']]],
   ['void_2',['Void',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a81ceb48a978444906d80119200aa358d',1,'Lanter::Message']]],
-  ['voidarray_3',['VoidArray',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a711442470617c351dc1dc8dcc07792e7',1,'Lanter::Message::Response']]],
-  ['voidcount_4',['VoidCount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a07be43db4e40e0e7c56790bf08064d63',1,'Lanter::Message::Response']]],
-  ['voidpartialsale_5',['VoidPartialSale',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597aacd265c3107a9b6cf71fcd4b1e3f0311',1,'Lanter::Message']]],
-  ['voidpreauth_6',['VoidPreAuth',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a43d6624113748b816a5af013c133d051',1,'Lanter::Message']]]
+  ['voidamount_3',['VoidAmount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a429e984e859cf85551832b503337b4e0',1,'Lanter::Message::Response']]],
+  ['voidarray_4',['VoidArray',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a711442470617c351dc1dc8dcc07792e7',1,'Lanter::Message::Response']]],
+  ['voidcount_5',['VoidCount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a07be43db4e40e0e7c56790bf08064d63',1,'Lanter::Message::Response']]],
+  ['voidpartialsale_6',['VoidPartialSale',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597aacd265c3107a9b6cf71fcd4b1e3f0311',1,'Lanter::Message']]],
+  ['voidpreauth_7',['VoidPreAuth',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a43d6624113748b816a5af013c133d051',1,'Lanter::Message']]]
 ];
--- doxygen/search/enumvalues_12.js
+++ doxygen/search/enumvalues_12.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['void_0',['Void',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a81ceb48a978444906d80119200aa358d',1,'Lanter::Message']]],
-  ['voidarray_1',['VoidArray',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a711442470617c351dc1dc8dcc07792e7',1,'Lanter::Message::Response']]],
-  ['voidcount_2',['VoidCount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a07be43db4e40e0e7c56790bf08064d63',1,'Lanter::Message::Response']]],
-  ['voidpartialsale_3',['VoidPartialSale',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597aacd265c3107a9b6cf71fcd4b1e3f0311',1,'Lanter::Message']]],
-  ['voidpreauth_4',['VoidPreAuth',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a43d6624113748b816a5af013c133d051',1,'Lanter::Message']]]
+  ['voidamount_1',['VoidAmount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a429e984e859cf85551832b503337b4e0',1,'Lanter::Message::Response']]],
+  ['voidarray_2',['VoidArray',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a711442470617c351dc1dc8dcc07792e7',1,'Lanter::Message::Response']]],
+  ['voidcount_3',['VoidCount',['../namespace_lanter_1_1_message_1_1_response.html#ab92cdd52172d3e9ae313e969a1700ef7a07be43db4e40e0e7c56790bf08064d63',1,'Lanter::Message::Response']]],
+  ['voidpartialsale_4',['VoidPartialSale',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597aacd265c3107a9b6cf71fcd4b1e3f0311',1,'Lanter::Message']]],
+  ['voidpreauth_5',['VoidPreAuth',['../namespace_lanter_1_1_message.html#ac71d410b44a029d19e6fc3008bc42597a43d6624113748b816a5af013c133d051',1,'Lanter::Message']]]
 ];
